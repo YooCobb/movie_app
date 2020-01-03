bffab5ecff438e409d3f950f9052c72e80fb6d9c
YooCobb <yhj010597@naver.com>
2020-01-03 20:29:00 +0900

    Updates

---
+++ precache-manifest.4dcf56ff79159c5dcfb76418f5b44258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da0ae79b23f6a6091564185048fed85",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0d69f77191eb49a91cfc",
+    "url": "/movie_app/static/css/main.64f25677.chunk.css"
+  },
+  {
+    "revision": "a3e54ecb9b384753c095",
+    "url": "/movie_app/static/js/2.a54d7e75.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app/static/js/2.a54d7e75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d69f77191eb49a91cfc",
+    "url": "/movie_app/static/js/main.3ccda75f.chunk.js"
+  },
+  {
+    "revision": "c14eec7ed0651debe84c",
+    "url": "/movie_app/static/js/runtime-main.b9d7b04d.js"
+  }
+]);
